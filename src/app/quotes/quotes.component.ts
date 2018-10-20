86f36485f354288ae10149255d00c3cc64330555
Marvin Chomba <marvin.chomba24@gmail.com>
2018-10-20 13:18:25 +0300

    Change the fonts and background colors

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,7 +11,8 @@ export class QuotesComponent implements OnInit {
   quotesList = [
     new Quotes("If you don't know where you're going, DON'T GO","Marvin Chomba",0,0,new Date(2018,9,17),"Marvin"),
     new Quotes("Coding is like toothpaste","Raphael Katana",0,0,new Date(2018,9,19),"Zig Ziglar"),
-    new Quotes("Playing football is simple, but playing simple football is the hardest thing","Jeremy Kimotho",0,0,new Date(2018,9,19),"Amira Mugure")
+    new Quotes("My favourite childhood memory is not paying bills","Jeremy Kimotho",0,0,new Date(2018,9,20),"Marvin Chomba"),
+    new Quotes("Life doesn't have hands but it can sure give you a slap sometimes","Unknown Genius",0,0,new Date(2018,9,20),"Nits")
   ]
   add(quote) {
     this.quotesList.unshift(quote);
