8fcad10b2e1ce25db7af5bd43cc81f4b6c86f83f
Marvin Chomba <marvin.chomba24@gmail.com>
2018-10-20 16:12:37 +0300

    Add responsiveness

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,8 +11,7 @@ export class QuotesComponent implements OnInit {
   quotesList = [
     new Quotes("If you don't know where you're going, DON'T GO","Marvin Chomba",0,0,new Date(2018,9,17),"Marvin"),
     new Quotes("Coding is like toothpaste","Raphael Katana",0,0,new Date(2018,9,18),"Zig Ziglar"),
-    new Quotes("Manchester United is better than Chelsea","No One Ever",0,0,new Date(2018,9,19),"Marvin Chomba"),
-    new Quotes("Life doesn't have hands but it can sure give you a slap sometimes","Nitsana",0,0,new Date(2018,9,20),"Sky")
+    new Quotes("Manchester United is better than Chelsea","No One Ever",0,0,new Date(2018,9,19),"Marvin Chomba")
   ]
   add(quote) {
     this.quotesList.unshift(quote);
