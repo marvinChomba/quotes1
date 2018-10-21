fe182f70d0852e282492cd4e6bcf113b5ffbcbc7
Marvin Chomba <marvin.chomba24@gmail.com>
2018-10-21 16:42:41 +0300

    Update the pipe

--- src/app/date-diff.pipe.ts
+++ src/app/date-diff.pipe.ts
@@ -8,9 +8,9 @@ export class DateDiffPipe implements PipeTransform {
   transform(value: any): any {
     let now:any = new Date();
     var diff = now.getTime() - value.getTime();
-    diff = diff / 1000;
-    diff = diff / (60*60)
-    diff = diff / 24;
+    diff/=1000;
+    diff/=(60*60)
+    diff/=24;
     return Math.ceil(diff) - 1;
   }
 }
