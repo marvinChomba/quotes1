36f187c79a1ee0ac7def345aa5be363daffaf3b2
Marvin Chomba <marvin.chomba24@gmail.com>
2018-10-21 11:10:51 +0300

    Change the order of quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,9 +9,9 @@ import { Quotes } from "../quotes"
 export class QuotesComponent implements OnInit {
 
   quotesList = [
-    new Quotes("If you don't know where you're going, DON'T GO","Marvin Chomba",0,0,new Date(2018,9,17),"Marvin"),
+    new Quotes("If you don't know where you're going, DON'T GO","Marvin Chomba",0,0,new Date(2018,9,19),"Marvin"),
     new Quotes("Coding is like toothpaste","Raphael Katana",0,0,new Date(2018,9,18),"Zig Ziglar"),
-    new Quotes("Manchester United is better than Chelsea","No One Ever",0,0,new Date(2018,9,19),"Marvin Chomba")
+    new Quotes("Manchester United is better than Chelsea","No One Ever",0,0,new Date(2018,9,17),"Marvin Chomba")
   ]
   highestArr:any[]
   highestVotes() {
